0c227ec257ef6a9782f0fec51eb0a4d4b352e0d2
GEORG-DESKTOP\Georg <georg.ehrler@s2018.tu-chemnitz.de>
2021-09-06 23:10:11 +0200

    added arduino project

---
+++ sw/matrix/Effect.h
@@ -0,0 +1,48 @@
+/*
+ * The Effect abstract class for the GauntletII project.
+ */
+
+#ifndef Effect_h
+#define Effect_h
+
+#define IR_PIN          2
+#define BUTTON_PIN      3
+#define MATRIX_PIN      6
+#define BRIGHTNESS_PIN  A5
+#define NUM_ANIMATIONEN 9
+#define HEIGHT          10
+#define WIDTH           10
+#define NUM_LEDS        100
+
+#define SLEEP_COOLDOWN  1000
+#define IR_COOLDOWN     700
+#define BUTTON_BOUNCE   50
+
+#import <FastLED.h>
+
+class Effect {
+
+    protected:
+        CRGB *leds;
+        int width, height;
+
+        //int _waitOffset;
+        //int _brightnessOffset;
+        //long _animationState;
+        bool _waitAndCheck(bool *_buttonSignal, bool *_IRSignal, long *_IRValue, volatile long *_IRValueNew, long *_waitOffset, int *_brightnessOffset, int *_animationState, unsigned long wait);
+        void _setBrightness(int *_brightnessOffset);
+        
+        struct CRGB& pixel(int x, int y);
+        bool inXRange(int x);
+        bool inYRange(int y);
+    
+        void clearLeds();
+
+    public:
+        Effect(CRGB *leds, int width, int height);
+
+        virtual void start() {};
+
+};
+
+#endif
