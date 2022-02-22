b127d838fb0345d8dcc0198c2c066d4ecc111517
Georg Ehrler <georg.ehrler@s2018.tu-chemnitz.de>
2022-02-22 01:47:36 +0100

    update root dir for new location

--- sw/matrix/root.h
+++ sw/matrix/root.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PROJECT_ROOT C:\Users\Georg\Documents\led-matrix\sw
+#define PROJECT_ROOT C:\dev\led-matrix\sw
 
 #define TO_STRING(s) #s
 #define ABSOLUTE_PATH(root, relative_path) TO_STRING(root\relative_path)
